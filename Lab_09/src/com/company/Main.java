c7fa9e2d8829b6f5bfa50de1ecc805df5ad13685
hlebs <glebszypula1997@gmail.com>
2020-12-01 14:46:30 +0100

    Edit lab 9

--- Lab_09/src/com/company/Main.java
+++ Lab_09/src/com/company/Main.java
@@ -1,10 +1,20 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        String[] strArr = splitArgs(args[0]);
+
+        System.out.println(strArr);
 
 
+    }
+
+    public static String[] splitArgs(String arg) {
+        return Arrays.stream(arg.split("[\\s+(),]")).
+                filter(w -> !w.isEmpty()).toArray(String[]::new);
     }
 }
